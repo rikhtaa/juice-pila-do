6e6e04efb310467d097437a1c13ba1d470dd389a
codewithkhadija <kahadijaabdulgafar@gmail.com>
2024-11-22 11:29:48 +0500

    Update App.jsx

--- my project/src/App.jsx
+++ my project/src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 import { Hero } from './components/Hero'
 import Nav from './components/Nav'
-import Btn from './components/Btn'
+import { Btn } from './components/Btn'
 function App() {
   return (
     <div>
